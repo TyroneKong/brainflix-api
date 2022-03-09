59d4a020f710407d2d5587cd9aa45beb8f341d71
TyroneKong <tyronekong@gmail.com>
2022-03-09 14:29:26 +0000

    set up routes

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const express = require('express')
 const PORT = 8010
 const app = express()
 const videoRoutes = require('./routes/videos')
-
+const cors = require('cors')
 
 //middleware
 app.use(express.json())
 app.use(express.static('public'))
-
+app.use(cors())
 //custom middleware
 
 app.use((req, res, next)=>{
     console.log('incoming request')
 next()
 })
-app.use('/videos', videoRoutes)
+app.use('/', videoRoutes)
 
 
 //home route
--- routes/videos.js
+++ routes/videos.js
@@ -1,19 +1,29 @@
-const express = require('express')
-const fs = require('fs')
-const router = express.Router()
+const e = require("express");
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
 
+//videos route
+router.get("/videos", (req, res) => {
+  fs.readFile("./data/videos.json", "utf-8", (err, data) => {
+    if (err) {
+      res.send("error reading videos data");
+    } else {
+      res.send(data);
+    }
+  });
+});
 
+//videos returned based off id
+router.get("/videos/:id", (req, res) => {
+  fs.readFile("./data/videos.json", "utf-8", (err, data) => {
+    if (err) {
+      res.send("error reading videos data");
+    } else {
+      const videoObj = JSON.parse(data);
+      res.send(videoObj.filter((video) => video.id === req.params.id));
+    }
+  });
+});
 
-router.get('/', (req, res)=>{
-    fs.readFile('./data/videos.json', 'utf-8', (err, data)=>{
-        if(err){
-            res.send('error reading videos data')
-        } else{
-            res.send(data)
-        }
-    })
-})
-
-
-
-module.exports = router
+module.exports = router;
